79e5f1e18c2ebd75cf2a674da2c16f4a571a6597
Mashal <azhar.mashal@gmail.com>
2018-05-20 13:23:53 -0400

    Sign up links show on sign_in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,11 @@
-Rails.application.routes.draw do 
-  get 'activities/new'
-  get 'activities/new' 
-  get 'schedules/index' 
-  get 'schedules/new' 
-  get 'schedules/show' 
+Rails.application.routes.draw do  
   devise_for :users 
-  root to: "home#index" 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
-  root to: "schedules#index" 
+  root "schedules#index" 
  
-  get 'home/home' 
+  # get 'home/home' 
   # devise_for :users 
   get 'sessions/new' 
- 
-  # root :to => redirect("/users/sign_in") 
-  # root to: 'devise/sessions#new' 
- 
-  # devise_for :users 
-  #   devise_scope :user do 
-     
-  #     authenticated :user do 
-  #     #modify to set the right homepage 
-  #     root 'schedules#index', as: :authenticated_root 
-  #   end 
- 
-  #   unauthenticated do 
-  #     root 'devise/sessions#new', as: :unauthenticated_root 
-  #   end 
-  # end 
+  get 'users/sign_in'
  
   resources :schedules 
   resources :activities 
