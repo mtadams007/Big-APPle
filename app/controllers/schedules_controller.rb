4f239289fb8be87e0f8acbba159cf8e6bd6e42e7
Mashal <azhar.mashal@gmail.com>
2018-05-20 12:59:51 -0400

    Repushed previous changes

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,10 +1,9 @@
 class SchedulesController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     # the header saves the api token, Authorization => Bearer makes it a bearer token
     @museums = HTTParty.get('https://api.yelp.com/v3/businesses/search?categories=museums&location=11238', headers: {"Authorization" => "Bearer #{ENV['YELP_API_KEY']}"})
-    puts @museums
-    puts '.....................'
-    puts '.....................'
     puts '.....................'
     puts @museums
   end
@@ -14,14 +13,6 @@ class SchedulesController < ApplicationController
     puts "*********************************"
 
   end 
-
-    # def search
-
-    #   parameters = { term: params[:term], limit: 16 }
-
-    #   render json: Yelp.client.search(‘San Francisco’, parameters)
-
-    # end
 
   def new
   end
