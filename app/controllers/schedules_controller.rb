cb54d180c24db924a86bf3b7baa47b8fbdb1bf78
Mashal <azhar.mashal@gmail.com>
2018-05-21 17:57:06 -0400

    Auto stash before merge of "mashal" and "origin/master"

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -10,13 +10,8 @@ class SchedulesController < ApplicationController
     @restaurant = HTTParty.get("https://api.yelp.com/v3/businesses/search?categories=#{params[:lunch]}&location=#{@museums['businesses'][0]['location']['zip_code']}", headers: {"Authorization" => "Bearer #{ENV['YELP_API_KEY']}"})
     @museums2 = HTTParty.get("https://api.yelp.com/v3/businesses/search?term=#{params[:term2]}&categories=museums&location=#{params[:location]}", headers: {"Authorization" => "Bearer #{ENV['YELP_API_KEY']}"})
     @restaurant2 = HTTParty.get("https://api.yelp.com/v3/businesses/search?categories=#{params[:dinner]}&location=#{@museums2['businesses'][0]['location']['zip_code']}", headers: {"Authorization" => "Bearer #{ENV['YELP_API_KEY']}"})
-<<<<<<< Updated upstream
 
     redirect_to '/activities/new'
-=======
-      # puts params[:term]
-      # puts params[:location]
->>>>>>> Stashed changes
   end
 
   def new
