64a0549d5dcf347b16310bda0ca6ef106102eb4b
Michael Adams <mtadams007@gmail.com>
2018-05-23 14:15:50 -0400

    made changes to master

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -1,10 +1,11 @@
-  let counter = 0;
+let max = 4
+let counter = 0;
 $( document ).on('turbolinks:load', function() {
 
     $(".schedule-submit-button").click(function(){
        $(this).addClass("hideMe");
        counter++;
-       if (counter === 4){
+       if (counter === max){
          counter = 0;
        $("#profileLink").removeClass("hideMe");
      }
