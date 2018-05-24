391c2450010eca4a9a3c741929a7488344e15aa9
Michael Adams <mtadams007@gmail.com>
2018-05-24 11:02:07 -0400

    carousel deletion works, trying to make code DRY

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -1,10 +1,35 @@
-  let counter = 0;
+let max = 4
+let counter = 0;
 $( document ).on('turbolinks:load', function() {
 
-    $(".schedule-submit-button").click(function(){
-       $(this).addClass("hideMe");
+    $(".schedule-submit-button.museum1_carousel1").click(function(){
+       $(".museum1_carousel1").addClass("hideMe");
        counter++;
-       if (counter === 4){
+       if (counter === max){
+         counter = 0;
+       $("#profileLink").removeClass("hideMe");
+     }
+    });
+    $(".schedule-submit-button.museum2_carousel1").click(function(){
+       $(".museum2_carousel1").addClass("hideMe");
+       counter++;
+       if (counter === max){
+         counter = 0;
+       $("#profileLink").removeClass("hideMe");
+     }
+    });
+    $(".schedule-submit-button.restaurant1_carousel1").click(function(){
+       $(".restaurant1_carousel1").addClass("hideMe");
+       counter++;
+       if (counter === max){
+         counter = 0;
+       $("#profileLink").removeClass("hideMe");
+     }
+    });
+    $(".schedule-submit-button.restaurant2_carousel1").click(function(){
+       $(".restaurant2_carousel1").addClass("hideMe");
+       counter++;
+       if (counter === max){
          counter = 0;
        $("#profileLink").removeClass("hideMe");
      }
