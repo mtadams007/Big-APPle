5f0193c89f05645b353a465d5db2d6026005c8ae
Michael Adams <mtadams007@gmail.com>
2018-05-24 11:27:53 -0400

    eureka

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -22,47 +22,4 @@ for(let  k=1; k<=num_of_business;k++){
   }
 }
 
-
-
-
-
-
-    // $(".schedule-submit-button.museum1_carousel1").click(function(){
-    //    $(".museum1_carousel1").addClass("hideMe");
-    //    counter++;
-    //    if (counter === max){
-    //      counter = 0;
-    //    $("#profileLink").removeClass("hideMe");
-    //  }
-    // });
-    // $(".schedule-submit-button.museum2_carousel1").click(function(){
-    //    $(".museum2_carousel1").addClass("hideMe");
-    //    counter++;
-    //    if (counter === max){
-    //      counter = 0;
-    //    $("#profileLink").removeClass("hideMe");
-    //  }
-    // });
-    $(".schedule-submit-button.restaurant1_carousel1").click(function(){
-       $(".restaurant1_carousel1").addClass("hideMe");
-       counter++;
-       if (counter === max){
-         counter = 0;
-       $("#profileLink").removeClass("hideMe");
-     }
-    });
-    $(".schedule-submit-button.restaurant2_carousel1").click(function(){
-       $(".restaurant2_carousel1").addClass("hideMe");
-       counter++;
-       if (counter === max){
-         counter = 0;
-       $("#profileLink").removeClass("hideMe");
-     }
-    });
-
-
-
-
-
-
 });
