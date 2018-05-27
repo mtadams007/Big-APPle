e453452d85c6955f483a8f8eab212c7236ee0841
Michael Adams <mtadams007@gmail.com>
2018-05-27 16:28:50 -0400

    deleted most let statements

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -5,14 +5,14 @@ $( document ).on('turbolinks:load', function() {
 num_of_caro = 5
 num_of_business = 4
 
-for(let  k=1; k<=num_of_business;k++){
-  for(let i=1;i<=num_of_caro;i++){
+for(k=1; k<=num_of_business;k++){
+  for(i=1;i<=num_of_caro;i++){
       $(`.schedule-submit-button.business${k}_carousel${i}`).click(function(){
         $(`.business${k}_carousel${i}`).addClass("hideMe");
         counter++;
         if (counter === maxActivities){
           counter = 0;
-          let new_url = window.location.origin;
+          new_url = window.location.origin;
           new_url += "/schedules/latest"
           window.location = new_url
         }
