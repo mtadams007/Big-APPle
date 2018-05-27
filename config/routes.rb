7dbf6cb142108ac84c0147448e1549bec493015c
Michael Adams <mtadams007@gmail.com>
2018-05-27 16:59:11 -0400

    deleted some routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
 
   # get 'home/home'
   # devise_for :users
-  get 'sessions/new'
-  get 'users/sign_in'
+
   post 'activities/new'
   get 'schedules/choose_museums'
   get 'activities/new_choice'
