819db77e4548ea2eb7ff3576a2222e16ccdf9fc5
Michael Adams <mtadams007@gmail.com>
2018-05-27 16:37:07 -0400

    confused

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -1,18 +1,18 @@
-
+maxActivities = 4
+counter = 0;
 $( document ).on('turbolinks:load', function() {
-  maxActivities = 4
-  counter = 0;
+
 num_of_caro = 5
 num_of_business = 4
 
-for(k=1; k<=num_of_business;k++){
-  for(i=1;i<=num_of_caro;i++){
+for(let  k=1; k<=num_of_business;k++){
+  for(let i=1;i<=num_of_caro;i++){
       $(`.schedule-submit-button.business${k}_carousel${i}`).click(function(){
         $(`.business${k}_carousel${i}`).addClass("hideMe");
         counter++;
         if (counter === maxActivities){
           counter = 0;
-          new_url = window.location.origin;
+          let new_url = window.location.origin;
           new_url += "/schedules/latest"
           window.location = new_url
         }
